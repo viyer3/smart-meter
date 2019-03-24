72302b76d9b6a9b3c030fe52bf8b41d0f741b8fc
keaysma <michaelandrewkeays@gmail.com>
2019-03-24 00:50:31 -0400

    Added sidebar prototype

--- smart/src/App.js
+++ smart/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import logo from './logo.svg';
 import './App.css';
+import AppSidebar from './sidebar.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+		<div className="App">
+        	<header className="App-header">
+          		<img src={logo} className="App-logo" alt="logo" />
+          		<p>
+          			Edit <code>src/App.js</code> and save to reload.
+					Hello!
+          		</p>
+          		<a
+            		className="App-link"
+            		href="https://reactjs.org"
+            		target="_blank"
+            		rel="noopener noreferrer"
+          		>
+            		Learn React
+          		</a>
+       		 </header>
+
+		<AppSidebar/>
+
+		</div>
     );
   }
 }
