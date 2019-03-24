6a7c9221eedb6d35e61dae519b70691feef52d76
keaysma <michaelandrewkeays@gmail.com>
2019-03-24 02:04:20 -0400

    Added geolocation package

--- smart/src/gmr.js
+++ smart/src/gmr.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import {geolocated} from 'react-geolocated';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -17,12 +18,35 @@ class SimpleMap extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key:''}}
-          defaultCenter={this.props.center}
+          bootstrapURLKeys={{ key:'AIzaSyC02JJiLtNy8XKna8HF4fCnLTbZBUYvQzQ'}}
+          defaultCenter={{
+		  	lat: this.props.isGeolocationAvailable? 
+					(this.props.isGeolocationEnabled? 
+					 	this.props.coords ? 
+							this.props.coords.latitude
+							: 59.955413
+						: 59.955413) 
+					: 59.955413,
+			lng: this.props.isGeolocationAvailable? 
+					(this.props.isGeolocationEnabled? 
+					 	this.props.coords ? 
+							this.props.coords.longitude
+							: 30.33
+						: 30.33) 
+					: 30.33,
+		  }}
           defaultZoom={this.props.zoom}
         >
           <AnyReactComponent
-            lat={59.955413}
+            lat={
+					this.props.isGeolocationAvailable? 
+							(this.props.isGeolocationEnabled? 
+							 	this.props.coords ? 
+									this.props.coords.latitude
+									: 59.955413
+								: 59.955413) 
+							: 59.955413
+			}
             lng={30.337844}
             text={'Kreyser Avrora'}
           />
@@ -32,4 +56,10 @@ class SimpleMap extends Component {
   }
 }
 
-export default SimpleMap;
+
+export default geolocated({
+	positionOptions: {
+		enableHighAccuracy: false
+	},
+	userDecisionTimeout: 5000
+})(SimpleMap);
