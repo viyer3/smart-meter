efe7e86685018e39180979e26723dbb1f08f3a7f
Vijay Iyer <viyer3@gmu.edu>
2019-03-24 00:46:37 -0400

    Added google maps on deez nuts

--- smart/src/App.js
+++ smart/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import SimpleMap from './gmr.js';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,18 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <SimpleMap/>
         </header>
       </div>
     );
