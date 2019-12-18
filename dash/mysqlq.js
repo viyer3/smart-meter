77d51acbeb6cf256d1abcfc397c39c5d79a970fb
keaysma <mkeays@gmu.edu>
2019-12-18 00:38:07 -0500

    Reconstructed middle-end queries.

--- dash/mysqlq.js
+++ dash/mysqlq.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
 	host     : '172.17.0.2',
 	user     : 'root',
 	password : 'metre',
-	database : 'usr',
+	database : 'usr_clear',
 	insecureAuth: true,
 });
 
@@ -17,7 +17,7 @@ app.use(cors())
 // https://expressjs.com/en/guide/routing.html
 app.get('/test', (req, res) => {
 	console.log("Incoming test");
-	connection.query("SELECT * FROM user;", (err, result, fields) => {
+	connection.query("SELECT * FROM locale;", (err, result, fields) => {
 		if(err){
 			res.send(err);
 		}else{
@@ -26,37 +26,50 @@ app.get('/test', (req, res) => {
 	});
 });
 
-app.get('/transactions/:plate', function (req, res) {
-	//connection.connect();
-
-	connection.query('SELECT loc_id, start_time, MINUTE(TIMEDIFF(end_time, start_time))*rate AS Cost FROM user NATURAL JOIN transaction WHERE plate = "' + req.params['plate'] + '";', function (error, results, fields) {
-		if(error){
-			res.send(error);
+app.get('/user/:id', (req, res) => {
+	connection.query(`SELECT fname, lname FROM user WHERE user_id=${req.params['id']}`, (sqlerr, sqlres, sqlfields) => {
+		if(sqlerr){
+			res.send(sqlerr);
 		}else{
-			console.log(results);
-			res.send(results);
+			sqlres.forEach((_res) => {
+				res.send(_res['fname']);
+			});
 		}
 	});
-
-	//connection.end();
 });
 
-// https://expressjs.com/en/guide/routing.html
-app.get('/total/:id', function (req, res) {
-	console.log('connecting...');
-	//connection.connect();
-	console.log('connected');
+app.get('/sess', (req, res) => res.send("WIP"));
 
-	connection.query("SELECT fname, lname, SUM(MINUTE(TIMEDIFF(end_time, start_time)))*rate AS Balance FROM user NATURAL JOIN transaction NATURAL JOIN user as A WHERE A.id = " + req.params['id'] + ' GROUP BY plate;', function (error, results, fields) {
-		if(error){
-			res.send(error);
+app.get('/trans/:plate', (req, res) => {
+	connection.query('SELECT country, locale, start_datetime, ((TIME_TO_SEC(TIMEDIFF(end_datetime, start_datetime))/3600)*rate) AS Cost FROM user NATURAL JOIN transaction NATURAL JOIN location NATURAL JOIN locale WHERE plate = "' + req.params['plate'] + '";', (sqlerr, sqlres, sqlfields) => {
+		if(sqlerr){
+			res.send(sqlerr);
 		}else{
-			console.log(results);
-			res.send(results);
+			res.send(sqlres);
 		}
 	});
+});
 
-	//connection.end();
+//Notable queries:
+//Get total cost for all vehicles for a user
+//SELECT lname, SUM((TIME_TO_SEC(TIMEDIFF(end_datetime, start_datetime))/3600)*rate) AS cost FROM transaction JOIN user WHERE user_id=${} GROUP BY user_id;
+//Get total cost for a single vehicle by license plate
+//SELECT plate, SUM((TIME_TO_SEC(TIMEDIFF(end_datetime, start_datetime))/3600)*rate) AS cost FROM transaction JOIN user WHERE user_id=${} GROUP BY plate, user_id;
+//Get costs of each individual plate for a user, ship with user info
+//SELECT lname, plate, SUM((TIME_TO_SEC(TIMEDIFF(end_datetime, start_datetime))/3600)*rate) AS cost FROM transaction JOIN user WHERE user_id=${} GROUP BY plate, user_id;
+
+app.get('/acc/user/total/:id', (req, res) => {
+	connection.query(`SELECT SUM((TIME_TO_SEC(TIMEDIFF(end_datetime, start_datetime))/3600)*rate) AS cost FROM transaction JOIN user WHERE user_id=${req.params['id']};`, (sqlerr, sqlres, sqlfields) => {
+		if(sqlerr){res.send(sqlerr);
+		}else{res.send(sqlres);}
+	});
+});
+
+app.get('/acc/user/plates/:id', (req, res) => {
+	connection.query(`SELECT plate, SUM((TIME_TO_SEC(TIMEDIFF(end_datetime, start_datetime))/3600)*rate) AS cost FROM transaction JOIN user WHERE user_id=${req.params['id']} GROUP BY plate, user_id;`, (sqlerr, sqlres, sqlfields) => {
+		if(sqlerr){res.send(sqlerr);}
+		else{res.send(sqlres);}
+	});
 });
 
 // Start the server
