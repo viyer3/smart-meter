fa503eece67caf211f52bfd5a9f90a29081873cd
keaysma <mkeays@gmu.edu>
2019-12-25 20:03:18 -0500

    Added relative fetch path

--- smart/src/User.js
+++ smart/src/User.js
@@ -6,56 +6,74 @@ import SimpleMap from './gmr.js';
 import logo from './logo.svg';
 import './App.css';
 import AppSidebar from './sidebar.js';
+import base64 from 'base-64';
 
 class User extends Component {
 	constructor(props) {
-		super(props)
+		super(props);
 		this.state = {
-			user: []
+			renderState: 0,
+			error: "",
+			transactionTable: [],
 		}
-	}
-	componentDidMount() {
-		let self = this;
-		const { params } = self.props.match
-		//fetch('/transactions', {
-		fetch('http://localhost:3003/transactions/' + params.plate, {
-			method: 'GET',
-			//mode: 'no-cors',
-			headers: {
-				"Accept": "application/json"
-			}
-		}).then((response) => response.json())
-		.then((json) => {
-			console.log(json);
-			self.setState({user: json});
-		}).catch(err => {
-			console.log('caught it!',err);
-		})
-		/*fetch('http://localhost:3000/posts')
-		.then(response => response.json())
-		.then(posts => (this.setState({posts})))*/
-	}					   
-		
-	createTable = () => {
-		let table = []
-		for (let i = 0; i < this.state.user.length; i++) {
-			var cst = this.state.user[i].Cost
-			table.push(<tr><td>{this.state.user[i].start_time}</td><td>&nbsp;:&nbsp;&nbsp;</td><td>${cst.toString()}</td></tr>)
-		}
-		return table
 	}
 
-  render() {
-	const { params } = this.props.match
-    return (
-      <div className="App">
-        <header className="App-header">
-		  <p> Transactions for {params.plate}</p>
-		  {this.createTable()}
-        </header>
-      </div>
-    );
-  }
+	componentDidMount() {
+		var login = "smart@meter.com";
+		var password = "metre";
+		const { params } = this.props.match;
+
+		this.setState({renderState: 1});
+
+		fetch(`http://${window.location.hostname}:3003/trans/` + params.plate, {
+			method: 'GET',
+			//mode: 'no-cors',
+			headers: new Headers({
+				"Accept": "application/json",
+				"Authorization": `Basic ${base64.encode(`${login}:${password}`)}`
+			})
+		})
+		.then((response) => response.json())
+		.then((json) => {
+			console.log(json);
+			this.setState({
+				//Loaded
+				renderState : 2,
+				transactionTable: this.createTable(json)
+			});
+		})
+		.catch(err => {
+			console.log('caught it!',err);
+			this.setState({
+				renderState: -1,
+				error: err.message
+			});
+		})
+	}					   
+		
+	createTable = (data) => {
+		let table = [];
+		console.log(data);
+		for (let i = 0; i < data.length; i++) {
+			var cst = data[i].Cost;
+			table.push(<div>{data[i].start_datetime}: ${cst.toString()}</div>)
+		}
+		return table;
+	}
+
+	render() {
+		const { params } = this.props.match
+		return (
+			<header className="App-header">
+				<div className="App">
+					<p> Transactions for {params.plate}</p>
+					{this.state.renderState === -1	&& `An error occured: ${this.state.error}`}
+		  			{this.state.renderState === 1 	&& "Loading"}
+		  			{this.state.renderState === 2 	&& this.state.transactionTable}
+      				</div>
+        		</header>
+    		);
+  	}
 }
 
 export default User;
