7dc49edcbf339f8bfd82ec876ec936c1790f13d3
keaysma <mkeays@gmu.edu>
2020-01-12 23:37:08 -0500

    Clean up

--- Website/src/userdata.js
+++
@@ -1,6 +0,0 @@
-router.get('/transactions', function(req, res, next) {
-	res.locals.connection.query('select * from members', function (error, results, fields) {
-		if(error) throw error;
-		res.send(JSON.stringify(results));
-	});
-});
